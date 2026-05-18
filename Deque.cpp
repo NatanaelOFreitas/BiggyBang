bb2c14cde7d11a8823c88c8450bb190f8315ffcb
Natanael Freitas <naelflashtw@gmail.com>
2026-05-17 21:52:57 -0400

    finalizado

--- Deque.cpp
+++ Deque.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
 template<typename T>
 class Node {
 private:
     T item;
-
 public:
     Node* next;
     Node* back;
-
     Node() {
         next = nullptr;
         back = nullptr;
     }
-
     Node(T item_) {
         item = item_;
         next = nullptr;
         back = nullptr;
     }
-
     T& get_item() {
         return item;
     }
@@ -30,21 +28,17 @@ class ListNavigator {
 private:
     Node<T>* current;
     Node<T>* endNode;
-
 public:
     ListNavigator(Node<T>* start, Node<T>* end) {
         current = start;
         endNode = end;
     }
-
     bool hasNext() {
         return current != endNode;
     }
-
     T& getCurrent() {
         return current->get_item();
     }
-
     void next() {
         if (current != endNode) {
             current = current->next;
@@ -63,7 +57,6 @@ class Deque {
 private:
     Node<T>* first;
     Node<T>* last;
-
 public:
     Deque() {
         first = new Node<T>();
@@ -71,15 +64,12 @@ public:
         first->next = last;
         last->back = first;
     }
-
     Node<T>* getFirst() {
         return first;
     }
-
     Node<T>* getLast() {
         return last;
     }
-
     void setFront(T item) {
         Node<T>* aux = new Node<T>(item);
         aux->next = first->next;
@@ -87,7 +77,6 @@ public:
         first->next = aux;
         aux->back = first;
     }
-
     void setBack(T item) {
         Node<T>* aux = new Node<T>(item);
         aux->back = last->back;
@@ -95,18 +84,27 @@ public:
         aux->next = last;
         last->back = aux;
     }
-
     void removeFront() {
+        if(empty()) {
+            return;
+        }
         Node<T>* aux = first->next;
         first->next = aux->next;
         aux->next->back = first;
         delete aux;
     }
-
+    void removeBack() {
+        if(empty()) {
+            return;
+        }
+	    Node<T>* aux = last->back;
+	    last->back = aux->back;
+	    aux->back->next = last;
+	    delete aux;
+    }
     T& getItemFront() {
         return first->next->get_item();
     }
-
     ListNavigator<T> getListNavigator() {
         return ListNavigator<T>(first->next, last);
     }
@@ -117,36 +115,29 @@ class Queue {
 private:
     Deque<T> deque;
     int length;
-
 public:
     Queue() {
         length = 0;
     }
-
     void enqueue(T item) {
         deque.setBack(item);
         length++;
     }
-
     void dequeue() {
         if (!empty()) {
             deque.removeFront();
             length--;
         }
     }
-
     T& front() {
         return deque.getItemFront();
     }
-
     bool empty() {
         return length == 0;
     }
-
     int size() {
         return length;
     }
-
     QueueNavigator<T> getQueueNavigator() {
         return QueueNavigator<T>(deque.getFirst()->next, deque.getLast());
     }
@@ -157,36 +148,29 @@ class Stack {
 private:
     Deque<T> deque;
     int length;
-
 public:
     Stack() {
         length = 0;
     }
-
     void push(T item) {
         deque.setFront(item);
         length++;
     }
-
     void pop() {
         if (!empty()) {
             deque.removeFront();
             length--;
         }
     }
-
     T& top() {
         return deque.getItemFront();
     }
-
     bool empty() {
         return length == 0;
     }
-
     int size() {
         return length;
     }
-
     StackNavigator<T> getStackNavigator() {
         return StackNavigator<T>(deque.getFirst()->next, deque.getLast());
     }
@@ -196,12 +180,10 @@ class Command {
 public:
     char action;
     int id;
-
     Command() {
         action = '-';
         id = 0;
     }
-
     Command(char action_, int id_) {
         action = action_;
         id = id_;
@@ -210,26 +192,17 @@ public:
 
 bool existsInFep(Queue<Command>& fep, int id) {
     QueueNavigator<Command> navigator = fep.getQueueNavigator();
-
     while (navigator.hasNext()) {
         Command command = navigator.getCurrent();
-
         if (command.action == 'E' && command.id == id) {
             return true;
         }
-
         navigator.next();
     }
-
     return false;
 }
 
-void processBiggy(
-    Queue<Command>& commands,
-    Queue<Command>& fep,
-    Queue<int>& cancel,
-    Queue<int>& desc
-) {
+void processBiggy(Queue<Command>& commands, Queue<Command>& fep, Queue<int>& cancel, Queue<int>& desc) {
     while (!commands.empty()) {
         Command command = commands.front();
         commands.dequeue();
@@ -255,46 +228,29 @@ void processBiggy(
     }
 }
 
-void removeFromPep(
-    int id,
-    char action,
-    Stack<int>& pep,
-    Stack<int>& aux,
-    Queue<int>& cancel,
-    Queue<int>& desc
-) {
+void removeFromPep(int id, char action, Stack<int>& pep, Stack<int>& aux, Queue<int>& cancel, Queue<int>& desc) {
     while (!pep.empty() && pep.top() != id) {
         aux.push(pep.top());
         pep.pop();
     }
-
     if (!pep.empty()) {
         pep.pop();
-
         if (action == 'C') {
             cancel.enqueue(id);
         } else {
             desc.enqueue(id);
         }
     }
-
     while (!aux.empty()) {
         pep.push(aux.top());
         aux.pop();
     }
 }
 
-void processBang(
-    Queue<Command>& fep,
-    Stack<int>& pep,
-    Stack<int>& aux,
-    Queue<int>& cancel,
-    Queue<int>& desc
-) {
+void processBang(Queue<Command>& fep, Stack<int>& pep, Stack<int>& aux, Queue<int>& cancel, Queue<int>& desc) {
     while (!fep.empty()) {
         Command command = fep.front();
         fep.dequeue();
-
         if (command.action == 'E') {
             pep.push(command.id);
         } else if (command.action == 'C' || command.action == 'A') {
@@ -306,78 +262,58 @@ void processBang(
 }
 
 void printCommandQueue(const char* name, Queue<Command>& queue) {
-    std::cout << name << " = [";
-
+    cout << name << " = [";
     QueueNavigator<Command> navigator = queue.getQueueNavigator();
     bool first = true;
-
     while (navigator.hasNext()) {
         if (!first) {
-            std::cout << ", ";
+            cout << ", ";
         }
-
         Command command = navigator.getCurrent();
-        std::cout << "(" << command.action << ", " << command.id << ")";
-
+        cout << "(" << command.action << ", " << command.id << ")";
         first = false;
         navigator.next();
     }
-
     if (first) {
-        std::cout << " ";
+        cout << " ";
     }
-
-    std::cout << "]\n";
+    cout << "]\n";
 }
 
 void printIntQueue(const char* name, Queue<int>& queue) {
-    std::cout << name << " = [";
-
+    cout << name << " = [";
     QueueNavigator<int> navigator = queue.getQueueNavigator();
     bool first = true;
-
     while (navigator.hasNext()) {
         if (!first) {
-            std::cout << ", ";
+            cout << ", ";
         }
-
-        std::cout << navigator.getCurrent();
-
+        cout << navigator.getCurrent();
         first = false;
         navigator.next();
     }
-
     if (first) {
-        std::cout << " ";
+        cout << " ";
     }
-
-    std::cout << "]\n";
+    cout << "]\n";
 }
 
 void printStack(const char* name, Stack<int>& stack) {
-    std::cout << name << " = [";
-
+    cout << name << " = [";
     StackNavigator<int> navigator = stack.getStackNavigator();
     bool first = true;
-
     while (navigator.hasNext()) {
         if (!first) {
-            std::cout << ", ";
+            cout << ", ";
         }
-
-        std::cout << navigator.getCurrent();
-
+        cout << navigator.getCurrent();
         first = false;
         navigator.next();
     }
-
-    
-
     if (first) {
-        std::cout << " ";
+        cout << " ";
     }
-
-    std::cout << "]\n";
+    cout << "]\n";
 }
 
 int main() {
@@ -393,24 +329,22 @@ int main() {
     char action;
     int id;
 
-    while (std::cin >> action >> id) {
+    while (cin >> action >> id) {
         commands.enqueue(Command(action, id));
-
         if (action == '-') {
             break;
         }
     }
-
     processBiggy(commands, fep, cancel, desc);
 
-    std::cout << "Biggy:\n";
+    cout << "Biggy:\n";
     printCommandQueue("FEP", fep);
     printIntQueue("CANCEL", cancel);
     printIntQueue("DESC", desc);
 
     processBang(fep, pep, aux, cancel, desc);
 
-    std::cout << "\nBang:\n";
+    cout << "\nBang:\n";
     printCommandQueue("FEP", fep);
     printStack("PEP", pep);
     printStack("AUX", aux);
